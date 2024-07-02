e2c59c2739f047cc55920e0b8ac9ac0aad4f95db
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-07-02 14:36:16 +0300

    login form

--- website1/urls.py
+++ website1/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #path('login/', views.login_user, name='login'),
+    #path('logout/', views.logout_user, name='logout'),
 ]
