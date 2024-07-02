904c7ebe585646e18d23e05e0889c73fcacd1534
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-07-02 16:25:53 +0300

    login/logout forms

--- website1/views.py
+++ website1/views.py
@@ -20,8 +20,8 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You have been log out!')
+    return redirect('home')
