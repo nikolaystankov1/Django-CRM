dca8b3d88e6df5ad3ff44ee2ebc83816e0b75e39
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-07-03 13:13:00 +0300

    individual records

--- website1/views.py
+++ website1/views.py
@@ -47,3 +47,12 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
 
     return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Customer.objects.get(id=pk)
+        return render(request, 'customer.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view that page!')
+        return redirect('home')
